fac788e32ca87aad741f529f2ed51d01bb4ec193
GewoonRene <rhuibertsjr@gmail.com>
2020-04-30 11:49:47 +0200

    Fixed: Editor code compiler

--- src/containers/editor-container/EditorContainer.tsx
+++ src/containers/editor-container/EditorContainer.tsx
@@ -1,18 +1,52 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, createRef, RefObject } from "react";
 import s from './editor.module.less';
+
+import { inject, Workspace } from "blockly";
+import Compiler from 'blockly/javascript';
+import toolbox from './toolbox/toolbox.xml';
 
 class EditorContainer extends PureComponent
 {
+	private readonly editor: RefObject<HTMLDivElement>;
+	private workspace: Workspace | undefined;
+	
 	constructor(props: any)
 	{
 		super(props);
+		this.editor = createRef();
 	}
+	
+	public componentDidMount(): void
+	{
+		if (this.editor.current != null) {
+			// @ts-ignore
+			this.workspace = inject(this.editor.current, {
+				toolbox: toolbox,
+				move: {
+					scrollbars: false
+				}
+			})
+		} else throw new Error('Editor: cannot initialize the workspace');
+	}
+	
+	private onRunEventHandler = (): void => {
+		const code = Compiler.workspaceToCode(this.workspace);
+		console.log(code);
+	};
 	
 	public render(): JSX.Element
 	{
 		return (
 			<div className={s.appEditorContainer}>
-			
+				<div className={s.appRunButton}>
+					<button onClick={this.onRunEventHandler}>
+						Run
+					</button>
+				</div>
+				<div
+					className={s.appEditor}
+					ref={this.editor}
+				/>
 			</div>
 		);
 	}
--- src/containers/game-container/GameContainer.tsx
+++ src/containers/game-container/GameContainer.tsx
@@ -12,8 +12,6 @@ class GameContainer extends PureComponent
 		super(props);
 		this.gameRef = createRef();
 		this.game = new Application({
-			width: 500,
-			height: 500,
 			backgroundColor: 0x1099bb
 		});
 	}
