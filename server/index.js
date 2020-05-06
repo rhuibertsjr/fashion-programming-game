e9eed47634448a377c2ed74d8603e92f38321667
GewoonRene <rhuibertsjr@gmail.com>
2020-05-06 13:40:11 +0200

    fixes: server 1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 
+const path = require('path');
 const express = require("express");
 const app = express();
 
 const http = require('http');
 const server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static('client/build'));
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.sendFile(path.resolve('dist', 'index.html')));
 
 server.listen(PORT, () => {
 	console.log('Up and running...')
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 
-import React, { Fragment } from "react";
+import React from "react";
 import {render} from "react-dom";
 import './index.less';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import EditorContainer from "./containers/editor-container/EditorContainer";
 import GameContainer from "./containers/game-container/GameContainer";
 
 export const App = (): JSX.Element => (
-	<Fragment>
-		<EditorContainer />
-		<GameContainer />
-	</Fragment>
+	<Router>
+		<Route path="/">
+			<EditorContainer />
+			<GameContainer />
+		</Route>
+	</Router>
 );
 
 document.addEventListener('DOMContentLoaded', () => render(
