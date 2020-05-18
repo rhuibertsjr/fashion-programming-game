4c528e25b9006eff3ef1595318499a41f5cd0ec3
maessmit <maessmit@gmail.com>
2020-05-18 15:41:25 +0200

    change socket url

--- src/components/socket-client/SocketClient.tsx
+++ src/components/socket-client/SocketClient.tsx
@@ -1,6 +1,6 @@
 import socketIo from 'socket.io-client';
 
-const ENDPOINT = 'https://dresscode-fun.herokuapp.com/';
+const ENDPOINT = 'https://dresscode-fun.herokuapp.com:3000/';
 const socket = socketIo(ENDPOINT);
 
 export {socket}
