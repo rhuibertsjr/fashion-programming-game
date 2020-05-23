0b56c1868f257b7696916995e6ca83a96c34a31c
GewoonRene <rhuibertsjr@gmail.com>
2020-05-23 17:19:03 +0200

    Added: Modeshow.tsx

--- src/pages/modeshow/Modeshow.tsx
+++ src/pages/modeshow/Modeshow.tsx
@@ -2,6 +2,10 @@ import React, { Fragment } from 'react';
 import { Title, RankingComponent } from "@components/index";
 import s from './modeshow.module.less'
 
+import spotlight from '@assets/images/spotlight.png';
+import character_2 from "@assets/game/characters/character_1.png";
+import clothes from "@assets/game/clothes/clothes_1.png";
+
 export const Modeshow = (): JSX.Element =>
 {
 	return (
@@ -13,7 +17,19 @@ export const Modeshow = (): JSX.Element =>
 			/>
 			<RankingComponent />
 			<div className={s.appModeshowContainer}>
-			
+				<div className={s.appModeshow}>
+					<h1> [Character Name] </h1>
+					<div className={s.appModeshowFloor} />
+					<img className={s.appModeshowSpotlightFirst} src={spotlight} alt="Dresscode spotlight"/>
+					<img className={s.appModeshowSpotlightLast} src={spotlight} alt="Dresscode spotlight"/>
+					<div className={s.appModeshowUser}>
+						<img src={character_2} alt="Dresscode Character"/>
+						<img src={clothes} alt="Dresscode Character"/>
+					</div>
+				</div>
+				<div className={s.appFriends}>
+				
+				</div>
 			</div>
 		</Fragment>
 	);
