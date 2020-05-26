e21d6ba6d80d5fd3557b313d6b5001ffdbd2fad2
GewoonRene <rhuibertsjr@gmail.com>
2020-05-26 12:34:12 +0200

    Fixed: #37

--- server/index.js
+++ server/index.js
@@ -11,28 +11,6 @@ const io = socketIo(server);
 
 const router = require('./routes/router');
 
-const webpack = require('webpack');
-
-const compiler = webpack(require('../config/deploy.config.js'));
-
-app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true,
-	publicPath: '/'
-}));
-
-app.use('*', function (req, res, next) {
-	let filename = path.join(compiler.outputPath,'index.html');
-	compiler.outputFileSystem.readFile(filename, function(err, result){
-		if (err) {
-			return next(err);
-		}
-		res.set('content-type','text/html');
-		res.send(result);
-		res.end();
-	});
-});
-
-
 app.use(express.static('dist'));
 app.use(router);
 
