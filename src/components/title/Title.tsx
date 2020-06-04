b7ff86ff59979973844297e67e32c90d6af07bf6
GewoonRene <rhuibertsjr@gmail.com>
2020-06-04 20:48:37 +0200

    Styling chat goed aangepast

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './title.module.less';
+import {Link} from "react-router-dom";
 
 export const Title = (props: ITitleProps): JSX.Element => {
     let suffex: JSX.Element;
@@ -7,7 +8,9 @@ export const Title = (props: ITitleProps): JSX.Element => {
     if (!props.suffex) {
         return (
             <div className={s.appTitle}>
-                <h1> Dresscode </h1>
+	            <Link to="/">
+		            <h1> Dresscode </h1>
+	            </Link>
             </div>
 
         )
@@ -17,7 +20,9 @@ export const Title = (props: ITitleProps): JSX.Element => {
 
     return (
         <div className={s.appTitle}>
-            <h1> Dresscode </h1>
+            <Link to="/">
+	            <h1> Dresscode </h1>
+            </Link>
             {suffex}
         </div>
     );
