66cf7e5bef604da0798c9d1d93ed1fe0b5967e56
GewoonRene <rhuibertsjr@gmail.com>
2020-06-04 21:16:11 +0200

    Production update

--- src/components/socket-client/SocketClient.tsx
+++ src/components/socket-client/SocketClient.tsx
@@ -6,7 +6,7 @@ you can put this in the 'ENDPOINT'
 If you work local work with http://127.0.0.1:3000 or http://localhost:3000
 if you work or push to Heroku then use https://dresscode-fun.herokuapp.com/
  */
-const ENDPOINT = 'http://localhost:3000/#/';
+const ENDPOINT = 'https://dresscode-fun.herokuapp.com/';
 const socket = socketIo(ENDPOINT);
 
 
